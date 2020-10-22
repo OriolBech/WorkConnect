1533d78f4f2d737a75e8854636e32e034c40cefe
Your Name <you@example.com>
2020-10-22 17:36:35 +0200

    Role System

--- app/Http/Controllers/EmployeeController.php
+++ app/Http/Controllers/EmployeeController.php
@@ -13,19 +13,23 @@ class EmployeeController extends Controller
         $this->middleware('auth');
     }
 
-    public function index()
+    public function index(Request $request)
     {
+        $request->user()->authorizeRoles(['admin']);
         $data = Employee::orderBy('id','desc')->paginate(10)->setPath('Employees');
         return view('employees.index',compact(['data']));
     }
 
-    public function create()
-    {
+    public function create(Request $request)
+    {   
+        $request->user()->authorizeRoles(['admin']);
         return view('employees.create');
     }
 
     public function store(Request $request)
-    {
+    {   
+        $request->user()->authorizeRoles(['admin']);
+
         $request->validate([
          'name' => 'required',
          'lastname' => 'required',
@@ -36,21 +40,27 @@ class EmployeeController extends Controller
         Employee::create($request->all());
         return redirect()->back()->with('success','Create Successfully');
     }
-
+    
+    /*
     public function show($id)
     {
        $data =  Employee::find($id);
        return view('employees.show',compact(['data']));
     }
+    */
 
-    public function edit($id)
+    public function edit(Request $request, $id)
     {
-       $data = Employee::find($id);
-       return view('employees.edit',compact(['data']));
+        $request->user()->authorizeRoles(['admin']);
+    
+        $data = Employee::find($id);
+        return view('employees.edit',compact(['data']));
     }
 
     public function update(Request $request, $id)
     {
+        $request->user()->authorizeRoles(['admin']);
+
         $request->validate([
          'name' => 'required',
          'lastname' => 'required',
@@ -63,8 +73,10 @@ class EmployeeController extends Controller
         
     }
 
-    public function destroy($id)
-    {
+    public function destroy(Request $request, $id)
+    {   
+        $request->user()->authorizeRoles(['admin']);
+
         Employee::where('id',$id)->delete();
         return redirect()->back()->with('success','Delete Successfully');
     }
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        // first role seeder
+        $this->call(RoleTableSeeder::class);
+        // second user seeder
+        $this->call(UserTableSeeder::class);
     }
 }
